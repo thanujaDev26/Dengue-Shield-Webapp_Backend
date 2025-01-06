9a6b0d067b73ad49275d319e2874f4d6f379bae1
ksdkaveesha <149872713+ksdkaveesha@users.noreply.github.com>
2025-01-06 20:31:15 +0530

    Created

---
+++ src/main/java/com/dengue_webapp/dengue_webapp/DengueWebappApplication.java
@@ -0,0 +1,13 @@
+package com.dengue_webapp.dengue_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DengueWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DengueWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dengue_webapp/dengue_webapp/DengueWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengue_webapp.dengue_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DengueWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
