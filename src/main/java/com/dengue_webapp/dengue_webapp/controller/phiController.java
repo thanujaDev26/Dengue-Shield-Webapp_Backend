1b25b7b09ff597b19c19f4250a63327c0ec8f55c
ksdkaveesha <149872713+ksdkaveesha@users.noreply.github.com>
2025-01-14 12:49:58 +0530

    phi display ok

--- src/main/java/com/dengue_webapp/dengue_webapp/controller/phiController.java
+++ src/main/java/com/dengue_webapp/dengue_webapp/controller/phiController.java
@@ -1,5 +1,24 @@
 package com.dengue_webapp.dengue_webapp.controller;
+import com.dengue_webapp.dengue_webapp.service.phiService;
+import com.dengue_webapp.dengue_webapp.model.phi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/phi")
+@CrossOrigin
 
 public class phiController {
+    @Autowired
+    private phiService phiService;
 
+    @GetMapping("/display_phi")
+    public List<phi> phi_list(){
+        return phiService.getAllStudents();
+    }
 }
